f9e31f6eabbbd023a08f9d88daaae4b823c9f580
Malinda <malindafiv56@gmail.com>
2024-08-19 20:09:13 +0530

    post employee end point and frontend navbar

---
+++ employee/employee/src/main/java/com/employeeCrudApp/employee/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.employeeCrudApp.employee.controller;
+
+import com.employeeCrudApp.employee.entity.Employee;
+import com.employeeCrudApp.employee.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("./api")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public Employee postEmployee(@RequestBody Employee employee){
+
+        return employeeService.postEmployee(employee);
+
+    }
+
+}
--- employee/employee/src/main/java/com/employeeCrudApp/employee/service/EmployeeService.java
+++ employee/employee/src/main/java/com/employeeCrudApp/employee/service/EmployeeService.java
@@ -1,4 +1,18 @@
 package com.employeeCrudApp.employee.service;
 
+import com.employeeCrudApp.employee.employee.EmployeeRepository;
+import com.employeeCrudApp.employee.entity.Employee;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public Employee postEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
 }
