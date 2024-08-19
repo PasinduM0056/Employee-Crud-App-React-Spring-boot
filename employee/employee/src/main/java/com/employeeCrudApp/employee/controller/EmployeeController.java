39f7abbd403774f82bab33f10fc339e9b750c1a0
Malinda <malindafiv56@gmail.com>
2024-08-19 23:55:28 +0530

    backend employee controller update

--- employee/employee/src/main/java/com/employeeCrudApp/employee/controller/EmployeeController.java
+++ employee/employee/src/main/java/com/employeeCrudApp/employee/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.employeeCrudApp.employee.controller;
 import com.employeeCrudApp.employee.entity.Employee;
 import com.employeeCrudApp.employee.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("./api")
 @RequiredArgsConstructor
+@CrossOrigin
 public class EmployeeController {
 
     private final EmployeeService employeeService;
